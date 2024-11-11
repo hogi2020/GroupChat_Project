a5f822d1b4c45d27b940b8f61196196e134bfa89
DESKTOP-MI9FPSP\KOSMO <junmyung2020@gmail.com>
2024-11-11 12:49:47 +0900

    Update project file.
    Update project file.

---
+++ src/ProjectDBCode/MemDTO.java
@@ -0,0 +1,14 @@
+package ojmDB;
+
+import lombok.*;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+
+public class MemDTO {
+    // 선언부
+    private String mem_ip;
+    private String mem_nick;
+    private int mem_pw;
+}
