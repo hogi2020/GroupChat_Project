04c4280b5a091210835ce277dae524d2eb8ed545
DESKTOP-MI9FPSP\KOSMO <junmyung2020@gmail.com>
2024-11-13 18:49:55 +0900

    Update file.
    Update file.

--- src/ProjectDBCode/DBConnectionMgr.java
+++ src/ProjectDBCode/DBConnectionMgr.java
@@ -6,11 +6,7 @@ public class DBConnectionMgr {
 
     // Oracle DB Connection info
     public final String _DRIVER = "oracle.jdbc.driver.OracleDriver";
-<<<<<<< HEAD
     public final String _URL = "jdbc:oracle:thin:@192.168.0.36:1521:orcl11";
-=======
-    public final String _URL = "jdbc:oracle:thin:@localhost:1521:orcl11";
->>>>>>> origin/Junbyeong
     public final String _USER = "scott";
     public final String _PW = "tiger";
 
@@ -74,12 +70,4 @@ public class DBConnectionMgr {
             e.printStackTrace();
         }
     }
-<<<<<<< HEAD
-
-//    public static void main(String[] args) {
-//        DBConnectionMgr dbMgr = new DBConnectionMgr();
-//        dbMgr.getConnection();
-//    }
-=======
->>>>>>> origin/Junbyeong
 }
